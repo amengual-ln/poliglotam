bdd42eb1041cb8b61107810c150538a177c620df
Lautaro Amengual <amengual.ln@gmail.com>
2024-06-08 10:42:43 -0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<header class="border-t-8 border-[#E05900]">
+  <div class="flex justify-between items-center p-4 lg:px-32">
+    <img src="/logo.png" alt="Logo" class="h-12 lg:h-auto" />
+    <h1 class="opacity-80 hidden lg:block">EL MÉTODO DE UN POLÍGLOTA</h1>
+    <button class="bg-[#E05900] text-white font-medium px-6 py-4"
+      >COMIENZA HOY</button
+    >
+  </div>
+  <div class="hidden lg:flex justify-center bg-[#232323] p-4 text-white">
+    <nav class="flex list-none gap-16 cursor-pointer">
+      <li>INICIO</li>
+      <li>QUIENES SOMOS</li>
+      <li>IDIOMAS</li>
+      <li>EXPERIENCIAS</li>
+      <li>TRADUCCIONES</li>
+      <li>INFORMACIÓN</li>
+      <li>CONTACTO</li>
+    </nav>
+  </div>
+</header>
+<section>
+  <img src="/home-poliglotam.webp" alt="Hero" />
+</section>
