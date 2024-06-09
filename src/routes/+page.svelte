9d6a67aed8e6c355691ff0920f1d19cb807321fc
Lautaro Amengual <amengual.ln@gmail.com>
2024-06-09 12:34:42 -0300

    language select

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <header class="border-t-8 border-[#E05900]">
   <div class="flex justify-between items-center p-4 lg:px-32">
     <img src="/logo.png" alt="Logo" class="h-12 lg:h-auto" />
-    <h1 class="opacity-80 hidden lg:block">EL MÉTODO DE UN POLÍGLOTA</h1>
-    <button class="bg-[#E05900] text-white font-medium px-6 py-4"
-      >COMIENZA HOY</button
-    >
+    <h1 class="opacity-80 hidden lg:block font-medium">
+      EL MÉTODO DE UN POLÍGLOTA
+    </h1>
+    <div>
+      <button class="bg-[#E05900] text-white font-medium px-6 py-4"
+        >COMIENZA HOY</button
+      >
+      <select name="language" id="language" class="hidden lg:inline p-2 ml-2 bg-white">
+        <option value="spanish">Español</option>
+        <option value="english">English</option>
+      </select>
+    </div>
   </div>
   <div class="hidden lg:flex justify-center bg-[#232323] p-4 text-white">
     <nav class="flex list-none gap-16 cursor-pointer">
