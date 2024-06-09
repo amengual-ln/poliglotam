def7692a4c76bbe03db46ce7726d7112b08a40fe
Lautaro Amengual <amengual.ln@gmail.com>
2024-06-09 16:30:31 -0300

    parcial menu mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  let open = false;
+</script>
+
 <header class="border-t-8 border-[#E05900]">
   <div class="flex justify-between items-center p-4 lg:px-32">
     <img src="/logo.png" alt="Logo" class="h-12 lg:h-auto" />
@@ -8,14 +12,39 @@
       <button class="bg-[#E05900] text-white font-medium px-6 py-4"
         >COMIENZA HOY</button
       >
-      <select name="language" id="language" class="hidden lg:inline p-2 ml-2 bg-white">
+      <select
+        name="language"
+        id="language"
+        class="hidden lg:inline p-2 ml-2 bg-white"
+      >
         <option value="spanish">Español</option>
         <option value="english">English</option>
       </select>
     </div>
   </div>
-  <div class="hidden lg:flex justify-center bg-[#232323] p-4 text-white">
-    <nav class="flex list-none gap-16 cursor-pointer">
+  <div class="lg:flex justify-center bg-[#232323] lg:p-4 text-white">
+    <button
+      on:click={() => {
+        open = !open;
+      }}
+      class="lg:hidden text-3xl px-4 py-2"
+    >
+      =
+    </button>
+    <nav class="hidden lg:flex list-none gap-16 cursor-pointer">
+      <li>INICIO</li>
+      <li>QUIENES SOMOS</li>
+      <li>IDIOMAS</li>
+      <li>EXPERIENCIAS</li>
+      <li>TRADUCCIONES</li>
+      <li>INFORMACIÓN</li>
+      <li>CONTACTO</li>
+    </nav>
+  </div>
+  <div
+    class={`absolute w-full p-4 text-white text-center text-2xl ${open ? "block" : "hidden"}`}
+  >
+    <nav class="hidden lg:flex list-none gap-16 cursor-pointer">
       <li>INICIO</li>
       <li>QUIENES SOMOS</li>
       <li>IDIOMAS</li>
