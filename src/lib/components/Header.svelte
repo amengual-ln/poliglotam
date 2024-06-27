e64a7a895c745938420bd4512fe90ba20182ca7e
Lautaro Amengual <amengual.ln@gmail.com>
2024-06-27 01:44:11 -0300

    Terms in header menu and fix mobile menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script>
   let open = false;
+  let subMenuOpen = false;
 </script>
 
 <header class="border-t-8 border-[#E05900]">
@@ -32,13 +33,118 @@
       =
     </button>
     <nav class="hidden lg:flex list-none gap-16 cursor-pointer">
-      <li><a href="/">HOME</a></li>
-      <li><a href="/about-us">ABOUT US</a></li>
-      <li>LANGUAGES</li>
-      <li>EXPERIENCES</li>
-      <li>TRANSLATIONS</li>
-      <li>INFORMATION</li>
-      <li>CONTACT</li>
+      <li class="hover:text-[#E05900] transition"><a href="/">HOME</a></li>
+      <li class="hover:text-[#E05900] transition">
+        <a href="/about-us">ABOUT US</a>
+      </li>
+      <li class="hover:text-[#E05900] transition">LANGUAGES</li>
+      <li class="hover:text-[#E05900] transition">EXPERIENCES</li>
+      <li class="hover:text-[#E05900] transition">TRANSLATIONS</li>
+      <li class="hover:text-[#E05900] transition">INFORMATION</li>
+      <li class="hover:text-[#E05900] transition">CONTACT</li>
+      <li
+        on:mouseenter={() => (subMenuOpen = true)}
+        on:mouseleave={() => (subMenuOpen = !subMenuOpen)}
+        class="relative cursor-pointer"
+      >
+        <a href="/#ceo-de-tu-vida">TERMS</a>
+        <span class="ml-1">▼</span>
+        <div
+          class={`absolute w-48 top-6 bg-[#232323] p-4 gap-4 flex-col ${subMenuOpen ? "flex" : "hidden"}`}
+        >
+          <li class="hover:text-[#E05900] transition">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="/one-to-one-terms">ONE-TO-ONE TERMS</a
+            >
+          </li>
+          <li class="hover:text-[#E05900] transition">
+            <a target="_blank" rel="noopener noreferrer" href="/group-terms"
+              >GROUP TERMS</a
+            >
+          </li>
+        </div>
+      </li>
     </nav>
   </div>
+
+  <div>
+    <nav class=" lg:grid list-none gap-16 cursor-pointer"></nav>
+  </div>
 </header>
+
+<div
+  class={`absolute top-[140px] bg-[#232323] w-full p-4 text-white text-center text-2xl ${open ? "block" : "hidden"} z-10`}
+>
+  <ul class="flex flex-col gap-4 opacity-75 hover:opacity:100">
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        href="/">HOME</a
+      >
+    </li>
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        href="/about-us">ABOUT US</a
+      >
+    </li>
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        href="/#">LANGUAGES</a
+      >
+    </li>
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        href="/#">EXPERIENCES</a
+      >
+    </li>
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        href="/#">TRANSLATIONS</a
+      >
+    </li>
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        href="/#">CONTACT</a
+      >
+    </li>
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        target="_blank"
+        rel="noopener noreferrer"
+        href="/one-to-one-terms">ONE TO ONE TERMS</a
+      >
+    </li>
+    <li>
+      <a
+        on:click={() => {
+          open = !open;
+        }}
+        target="_blank"
+        rel="noopener noreferrer"
+        href="/group-terms">GROUP TERMS</a
+      >
+    </li>
+  </ul>
+</div>
